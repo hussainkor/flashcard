df34363ea258dc496d1a17e469db04a8098c601a
hussainkor <hussainkor@gmail.com>
2024-04-19 07:04:24 +0530

    Changed the UI of the theme

--- src/App.js
+++ src/App.js
@@ -1,66 +1,113 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from 'react'
+// import pizzaData from "./data"
 
-const data = [
-  {
-    id: 1,
-    question: 'React is the library of which language?',
-    answer: 'JavaScript',
-    married: false,
-    color: '#E0D8FC'
-  },
-  {
-    id: 2,
-    question: 'Who is the presidet of USA?',
-    answer: 'Joe Biden',
-    married: false,
-    color: '#DBFDD6'
-  },
-  {
-    id: 3,
-    question: 'How many characters are there in a PAN?',
-    answer: 10,
-    married: false,
-    color: '#FFBE59'
-  },
-  {
-    id: 4,
-    question: 'Maria Sharapova is related to which game?',
-    answer: 'Tennis',
-    married: false,
-    color: '#D3B9FB'
-  },
-  {
-    id: 5,
-    question: 'Who is the winner of cricket world cup 2023?',
-    answer: 'India',
-    married: false,
-    color: '#F1C5E0'
-  }
-]
+// https://dribbble.com/shots/22487680-Food-Order
+// https://dribbble.com/shots/20223703-Food-order-Mobile
 
-function App() {
-  return <>
-    <h1>Flashcard</h1>
-    <div className='box'>
-      <Flashcard />
-    </div></>
+export default function App() {
+    const [items, setItems] = useState([])
+
+    function handleAddItems(item) {
+        setItems(items => [...items, item])
+    }
+    function handleDeleteItem(id) {
+        setItems(items.filter((item) => item.id !== id))
+    }
+
+    function handleSoldOutItem(id) {
+        setItems(items.map((item) => item.id === id ? { ...item, soldOut: !item.soldOut } : item))
+    }
+
+    return (
+        <div className='item-box'>
+            <h1>Pizza Menu</h1>
+            <Headers onAddItems={handleAddItems} />
+            <Menu items={items} onDeleteItem={handleDeleteItem} onSoldOutItem={handleSoldOutItem} />
+            <Footers />
+        </div>
+    )
 }
 
-function Flashcard() {
-  const [selecedId, setSelectedId] = useState(null);
-  function clickHandler(id) {
-    setSelectedId(id !== selecedId ? id : null)
-  }
-
-  return <>
-    {
-      data.map((dat) =>
-        <div className='box-data' style={{ backgroundColor: dat.color }} key={dat.id} onClick={() => clickHandler(dat.id)}>
-          <h3>Q: {dat.question}</h3>
-          <p className={dat.id === selecedId ? 'active' : ''}>{dat.id === selecedId ? `Ans: ${dat.answer}` : ''}</p>
-        </div>)
-    }</>
+function Menu({ items, onDeleteItem, onSoldOutItem }) {
+    const pizzaNumb = items.length;
+    return <div className='item-list'>
+        <h2>Special Items for you</h2>
+        {pizzaNumb > 0 ? <div className="pizza-item">
+            {items.map((pizza) => (
+                <Pizza pizzaObj={pizza} key={pizza.name} onDeleteItem={onDeleteItem} onSoldOutItem={onSoldOutItem} />
+            ))}
+        </div> : <p>Oops no items available, check back later :)</p>
+        }</div>
 }
 
-export default App;
+function Pizza({ pizzaObj, onDeleteItem, onSoldOutItem }) {
+    const { title, imgUrl, ingredient, price, rating, soldOut, id } = pizzaObj;
+
+    return <div className={`pizza-list ${soldOut ? 'sold-out' : 'on-sell'} `}>
+        <img src={imgUrl} alt={title} />
+        <h3>
+            {title}
+            <input type='checkbox' value={soldOut} onChange={(e) => onSoldOutItem(id)} />
+        </h3>
+        <p>{ingredient}</p>
+        <p>
+            {soldOut ? 'Sold out' : `₹ ${price}`}
+            <span>{rating}</span> <span className="rating">★</span>
+        </p>
+        <button className='delete-btn' onClick={() => onDeleteItem(id)}>x</button>
+    </div>
+}
+
+function Headers({ onAddItems }) {
+    const [imgUrl, setImgUrl] = useState('');
+    const [title, setitle] = useState('');
+    const [ingredient, setIngredient] = useState('');
+    const [price, setPrice] = useState('');
+    const [formControl, setFormControl] = useState(true);
+
+    function handleAddItemSubmit(e) {
+        e.preventDefault();
+        if (!imgUrl || !title || !ingredient) return;
+
+        const newItem = { imgUrl, title, ingredient, price, rating: 4.5, soldOut: false, id: Date.now() }
+        onAddItems(newItem);
+
+        setImgUrl('');
+        setitle('');
+        setIngredient('');
+        setPrice('');
+    }
+    return <div className="main-header">
+
+        <div className="add-item">
+            <h2>
+                Add Items
+                <button onClick={() => setFormControl((s) => s = !s)} className='close-btn'>{formControl ? 'Close' : 'Open'}</button>
+            </h2>
+            {formControl && <form onSubmit={handleAddItemSubmit}>
+                <input type="text" placeholder="Enter Image Url" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)} />
+                <input type="text" placeholder="Enter title" value={title} onChange={(e) => setitle(e.target.value)} />
+                <input type="text" placeholder="Enter ingredients" value={ingredient} onChange={(e) => setIngredient(e.target.value)} />
+                <input type="number" placeholder="Enter price" value={price} onChange={(e) => setPrice(Number(e.target.value))} />
+                <button className='submit-btn'>Submit</button>
+            </form>}
+        </div>
+    </div>
+}
+
+function Footers() {
+    const hours = new Date().getHours();
+    const openHour = 10;
+    const closeHour = 22;
+    const isOpen = hours >= openHour && hours <= closeHour;
+    return <div className="feeter">
+        {isOpen ? <OpenFooter openedHour={openHour} closedHour={closeHour} /> :
+            <p>We are happy to welcome you between {openHour}:00 to {closeHour}:00</p>}
+    </div>
+}
+
+function OpenFooter({ openedHour, closedHour }) {
+    return <>
+        <p>{new Date().toLocaleTimeString()}. We're open from {openedHour}:00 to {closedHour}:00 Come visit us or order online.</p>
+        <button>Order Now</button></>
+}
---
+++ src/data.js
@@ -0,0 +1,52 @@
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        rating: 4.5,
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        rating: 4.5,
+        soldOut: true,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        rating: 4,
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        rating: 4.9,
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        rating: 4.2,
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        rating: 4.5,
+        soldOut: false,
+    },
+];
+
+export default pizzaData;
